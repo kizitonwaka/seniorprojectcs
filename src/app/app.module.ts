f9893210c2bce40587f81c3b96cce25d2c00a4af
Kizito Nwaka <kizito.nwaka@mvsu.edu>
2018-12-14 15:15:05 -0500

    estimator link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
@@ -8,8 +9,34 @@ import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { BrandsComponent } from './brands/brands.component';
+import { EstimatorComponent } from './estimator/estimator.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'estimator', component: EstimatorComponent },
+  { path: 'home', component: MainComponent },
+  /*{ path: 'hero/:id',      component: HeroDetailComponent },
+  {
+    path: 'heroes',
+    component: HeroListComponent,
+    data: { title: 'Heroes List' }
+  },
+  { path: '',
+    redirectTo: '/heroes',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }*/
+];
 
 @NgModule({
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    // other imports here
+    BrowserModule
+  ],
   declarations: [
     AppComponent,
     MainComponent,
@@ -17,10 +44,8 @@ import { BrandsComponent } from './brands/brands.component';
     FooterComponent,
     BodyComponent,
     CalculatorComponent,
-    BrandsComponent
-  ],
-  imports: [
-    BrowserModule
+    BrandsComponent,
+    EstimatorComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
