cbb722f93a93944be5f5a04442e49980049098b0
Kizito Nwaka <kizito.nwaka@mvsu.edu>
2018-12-13 20:38:02 -0500

    calculator body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { BrandsComponent } from './brands/brands.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BodyComponent,
+    CalculatorComponent,
+    BrandsComponent
   ],
   imports: [
     BrowserModule
