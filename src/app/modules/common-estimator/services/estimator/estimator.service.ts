2511c3d3a1d516b8f059dd644df7c5aa9b9e9264
Kizito, Nwaka <kizitonwaka@yahoo.com>
2019-05-02 17:13:50 -0500

    comments to service provider

--- src/app/modules/common-estimator/services/estimator/estimator.service.ts
+++ src/app/modules/common-estimator/services/estimator/estimator.service.ts
@@ -5,25 +5,32 @@ import { map } from 'rxjs/operators';
 import { Estimate } from '../../models/estimator.model';
 import { EstimateInterface, SalEstimateResponse, EstimateResponse } from '../../models/estimator.model';
 import {HttpClient} from '@angular/common/http'
+
+//http service provider
 @Injectable({
 	providedIn: 'root'
 })
 export class EstimatorService {
 
-uri = 'http://localhost:4000/estimate';
+  uri = 'http://localhost:4000/estimate';
 
   constructor(private http: HttpClient) { }
 
+  //adds collected data to our database, public
   addEstimate(obj) {
     console.log(JSON.stringify(obj));
     this.http.post(`${this.uri}/add`, obj)
         .subscribe(res => console.log('Done: '+JSON.stringify(obj)));
   }
-private getEstimates(est) {
-	let myEstimates = est.map((obj) => new Estimate(obj));
-	return myEstimates;
-}
-public all():Observable<Object>{
-	return this.http.get(`${this.uri}`)
+  
+  //collect previous estimate from our database
+  private getEstimates(est) {
+	  let myEstimates = est.map((obj) => new Estimate(obj));
+	  return myEstimates;
+  }
+  
+  //observe changes here
+  public all():Observable<Object>{
+	  return this.http.get(`${this.uri}`)
 	}
 }
