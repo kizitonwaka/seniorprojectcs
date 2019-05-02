a44908cac52141058e7eed6dca3f6033eaef1a8f
Kizito, Nwaka <kizitonwaka@yahoo.com>
2019-05-02 17:15:02 -0500

    comments to interfaces

--- src/app/modules/common-estimator/interfaces/estimate-response.ts
+++ src/app/modules/common-estimator/interfaces/estimate-response.ts
@@ -1,5 +1,6 @@
 import { EstimateInterface } from '../models/estimator.model';
 
+//our response from database should be an array of Estimate interfaces
 export interface EstimatorResponse {
     Estimator: Array<EstimateInterface>;
 }
